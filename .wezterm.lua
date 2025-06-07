c2c7afb551eaa39529cb9e64a9714b96a1b6aaae
ElectraBytes04 <electra-dev.404@protonmail.com>
2025-06-07 12:00:35 -0400

    Changed window padding to 5,5,5,5 in .wezterm.lua; Set gaps to inner 10 in i3/config; Set default monitor to HDMI-0 in polybar/config.ini; Moved theme line to bottom in rofi/config.rasi

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,10 +5,17 @@ if wezterm.config_builder then
 end
 
 -- Config start
-config.font_size = 13
+config.font_size = 12
 config.font = wezterm.font 'RobotoMono Nerd Font Mono'
 
 config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5
+}
 
 config.default_prog = {'/usr/bin/tmux'}
 
